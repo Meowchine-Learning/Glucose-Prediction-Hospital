b96d9b65410781512bd29bc6120e7de2bd22fd3a
Tur4L <tural.bakhtiyarli.2003@gmail.com>
2024-03-13 22:46:19 -0600

    build_features.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,56 +1,108 @@
 import pandas as pd
-import json
 
 
 def main():
 
     df_map = pd.read_excel(
-        "ACCESS 1853 Dataset.xlsx", sheet_name=None)
+        "data/ACCESS 1853 Dataset update 20240228.xlsx", sheet_name=None)
 
     encounters = df_map["ENCOUNTERS"]
     admit_dx = df_map["ADMIT_DX"]
-    OR_orders = df_map["OR_PROC_ORDERS"]
-    orders = df_map["ORDERS"]
-    orders_qs = df_map["ORDER_QUESTIONS"]
+    or_proc_orders = df_map["OR_PROC_ORDERS"]
+    orders_activity = df_map["ORDERS_ACTIVITY"]
+    orders_nutrition = df_map["ORDERS_NUTRITION"]
     labs = df_map["LABS"]
     med_admin = df_map["MEDICATION_ADMINISTRATIONS"]
-    med_orders = df_map["MEDICATION_ORDERS"]
     pin = df_map["PIN"]
 
+    clean_encounters(encounters)
     clean_admit(admit_dx)
-    df_map['LABS'] = clean_labs(labs)
+    clean_or_proc_orders(or_proc_orders)
+    clean_orders_activiy(orders_activity)
+    clean_orders_nutrition(orders_nutrition)
+    clean_labs(labs)
     clean_med_admin(med_admin)
+    clean_pin(pin)
 
     for key in df_map.keys():
         write_to_csv(df_map[key], key)
 
 
+def clean_encounters(df):
+    # drop A1C columns
+    df.drop(['PRE_ADMISSION_A1C', 'A1C_DAYS'], axis=1, inplace=True)
+
+    # find/remove rows with empty heights
+    empty_heights = df[(df['HEIGHT_CM'].notnull()) == False].index
+
+    df.drop(empty_heights, axis=0, inplace=True)
+
+
 def clean_admit(df):
+    '''
+        I found rows with missing ICD10 codes, I filled those missing codes using the rows's Admit_diag_text cells (or Dx_name)
+        I match the codes from Admit_Dx
+        Detaisl are in the dict below
+    '''
 
     # missing codes
     codes = {"Heart Failure": "I50.9", "Heart Failure, Pericardial Effusion": "I31.3, I50.9", "Critical Aortic Stenosis with Heart Failure": "I35.0, I50.9", "CHF": "I50.0",
-             "AORTIC STENOSIS": "I35.0",   # 10 & 11
+             "AORTIC STENOSIS": "I35.0",
              "STEMI": "I21.3, R94.30", "NSTEMI exacerbation": "I21.4, R94.31", "NSTEMI": "I21.4, R94.31", "Acute MI": "I21.9",
-             "Chest pain and SOB": "R07.4, R06.0", "Chest Pain": "R07.4", "SOB": "R06.0",  # 2
+             "Chest pain and SOB": "R07.4, R06.0", "Chest Pain": "R07.4", "SOB": "R06.0",
              "Infected Endocarditis": "I33.0", "Endocarditis": "I38",
 
              "Triple Vessel Disease": "I25.19", "3-vessel CAD": "I25.19", "Left Main CAD": "I25.19", "Coronary Artery Disease": "I25.19", "multivessel disease": "I25.19",
              "Lung Transplant": "Z94.2", "Lung Transplant.": "Z94.2", "Lung Tx.": "Z94.2", "Transplantation": "Z94.9",
-             "mitral regurgitation": "I34.0",  # 6
-             "Postop Sternal Pain": "R07.3", "Sternal wound infection": "S21.11", "Sternum infection": "S21.11", "Sternal infection": "S21.11",  # 3, 9
-             "Wound Infection": "T14.1, T79.3", "post op infection": "T81.4", "Driveline Infection": "T82.79, Y83.1",   # 4
+             "mitral regurgitation": "I34.0",
+             "Postop Sternal Pain": "R07.3", "Sternal wound infection": "S21.11", "Sternum infection": "S21.11", "Sternal infection": "S21.11",
+             "Wound Infection": "T14.1, T79.3", "post op infection": "T81.4", "Driveline Infection": "T82.79, Y83.1",
 
-             # 12
-             # 12
              "Cardioverter defibrillator subcutaneous insertion (SICD)": "Z45.01",
              "Fluid overload": "E87.7",
-             "Abscess to left thigh": "L02.4",
+             # "Abscess to left thigh": "L02.4",
              "Unstable angine": "I20.0",
              "Symptomatic Bradycardia": "R00.1",
              "Cardiogenic shock": "R57.0",
              "SAH": "I60.9",
              "ACS": "I24.9",
              "Afib, new onset": "I48.90",
+
+             "stroke": "I64",
+             "CABG (CORONARY ARTERY BYPASS GRAFT) [1070528]": "Z95.1", "CABG, WITH AORTIC VALVE REPLACEMENT": "Z95.1, Z95.4", "CABG": "Z95.1",
+             "NSTEMI/CABG": "Z95.1, I21.4, R94.31", "NSTEMI/wtg CABG": "Z95.1, I21.4, R94.31",
+             "Aortic Dissection": "I71.0", "MINI-STERNOTOMY AORTIC VALVE REPLACEMENT": "Z95.4", "REOPERATION, WITH AORTIC VALVE REPAIR OR REPLACEMENT [1072465]": "Z95.4", "Valve repair post op complication": "Y83.8",
+             "90% LM stenosis": "I35.0",
+             "Fluid overload VAD": "Z95",
+             "STERNAL WOUND": "S21.11", "Sternal wire infected": "T82.79",
+             "Pulmonary Fibrosis": "J84.9", "Pulmonary": "J44.9", "REPAIR, PARTIAL ANOMALOUS PULMONARY VENOUS RETURN": "Q24.8",
+             "Gram Positive Sepsis": "A41.9",
+             "HEART Transplant": "Z76.805", "Heart tx": "Z94.9",
+             "Acute MI and decompensated heart failure": "I50.0, I21.9",
+             "Double Lung Transplant": "Z94.2", "Preop lung tx": "Z76.803",
+             "MVR": "Z95.4",
+             "Fungal infection": "Z76.803",
+             "TAVR work up": "Z95.4",
+             "low hgb": "Z13.6",
+             "Generator change": "Z45.00",
+             "Pacemaker Problem": "Z45.00",
+             "Valvular heart failure": "I50.9",
+             "Coronary angiography +/- PCI": "Z13.6", "Coronary angiography W&R Cath from LAC LA BICHE; - non isolated, heparin gtt, 15/15, independent, RA": "Z13.6",
+             "Coronary angiography/possible PCI; Patient in Westlock hospital W&R Cath; - non isolated, RA, SL, able to lay flat, 15/15, independent": "Z13.6",
+             "Coronary angiography W&R Cath from Northern Lights Hospital 7807916296; - 48 Iso due to SOB and no Cough covid negative, independent, able to lay flat with pain management, 15/15": "Z13.6",
+             "LEFT HEART CATHETERIZATION +/- PCI": "4A023N", "Right heart catheterization": "4A023N6",
+             "VAD Workup": "Z13.6", "AVR May 11 - +/- VAD. VAD workup": "I35.9", "VAD / bradycardia": "Z95",
+             "DEBRIDEMENT, STERNUM, WITH REPAIR USING PLATE": "T81.4",
+             "Lung disease": "J98.4",
+             "Pre op MVR and lead extraction": "I05.9",
+             "EDPD": "Z76.803",
+             "Abd pain": "R10.4",
+             "Enlarging hematoma to left chest wall": "T82.9",
+             "Pump thrombosis": "T82.9",
+             "Bacteremia/Mitral Vegetation": "I05.9",
+             "Penetrating Ulcer - Distal Aortic Arch": "I70.0",
+             "EXTRACTION, ELECTRODE LEAD, CARDIAC, USING LASER; \Reimplant of CRT-D with new RV and LV leads": "I05.9",
+             "liver biopsy VAD patient": "Z95", "VAD patient for generator change Monday": "Z45.00,Z95", "REMOVAL, ELECTRODE LEAD, ICD [1072379]": "I05.9", "VAD work- up": "Z13.6", "REMOVAL, ELECTRODE LEAD, ICD [1072379]": "I05.9"
              }
 
     # turn all to lowercase
@@ -75,56 +127,120 @@ def clean_admit(df):
             # drop rows where we don't know know the code
             df.drop(list[i], axis=0, inplace=True)
 
-    # QUESTION #2: Is SOB the same as SOBOE?    Not necesserily the same
-    # QUESTION #3: Is post op Postop Sternal Pain the same as sternal pain? probably
-    # QUESTION #4: Is Driveline Infection same as Infection associated with driveline of left ventricular assist device (LVAD)? probably
-    # QUESTION #5: Is Fluid overload VAD same as Fluid overload?    different
-    # QUESTION #6: Is Bacteremia/Mitral Vegetation same as Mitral regurgitation?    different
-    # QUESTION #7: what does MVR stand for (repair, replacement, regurgitation)?    send it here
-    # QUESTION #8: Is NSTEMI same as NSTEMI exacerbation?   probably the same
-    # QUESTION #9: Is sternum infection same as sternum wound infection?    yes
-    # QUESTION #10: "Heart Failure, Pericardial Effusion" same as composite of two?
-    # QUESTION #11: Is Valvular heart failure same as Valvular heart disease?   different
-    # QUESTION #12: Is "Cardioverter defibrillator subcutaneous insertion (SICD)" same code as Fitting or adjustment of automatic implantable cardioverter-defibrillator?   same
+    df.drop('ADMIT_DIAG_TEXT', axis=1, inplace=True)
+    df.drop('DX_NAME', axis=1, inplace=True)
 
-    # "stroke":"",
-    # "CABG (CORONARY ARTERY BYPASS GRAFT) [1070528]":"", "CABG, WITH AORTIC VALVE REPLACEMENT":"",
-    # "Aortic Dissection":"", "MINI-STERNOTOMY AORTIC VALVE REPLACEMENT", "Penetrating Ulcer - Distal Aortic Arch", "REOPERATION, WITH AORTIC VALVE REPAIR OR REPLACEMENT [1072465]", "Valve repair post op complication",
-    # "Coronary angiography +/- PCI", "Coronary angiography W&R Cath from LAC LA BICHE; - non isolated, heparin gtt, 15/15, independent, RA", "Coronary angiography/possible PCI; Patient in Westlock hospital W&R Cath; - non isolated, RA, SL, able to lay flat, 15/15, independent", "90% LM stenosis", "Coronary angiography W&R Cath from Northern Lights Hospital 7807916296; - 48 Iso due to SOB and no Cough covid negative, independent, able to lay flat with pain management, 15/15",
-    # "VAD Workup", "Fluid overload VAD", "AVR May 11 - +/- VAD. VAD workup":"", "VAD / bradycardia",  # 5
-    # "STERNAL WOUND", "DEBRIDEMENT, STERNUM, WITH REPAIR USING PLATE", "Sternal wire infected",
-    # "Pulmonary Fibrosis", "Pulmonary","REPAIR, PARTIAL ANOMALOUS PULMONARY VENOUS RETURN",
-    # "LEFT HEART CATHETERIZATION +/- PCI": "", "Right heart catheterization":"",
-    # "HEART Transplant", "Acute MI and decompensated heart failure"
-    # "Lung disease", "Double Lung Transplant", "Preop lung tx",
-    # "Pre op MVR and lead extraction", "MVR",  # 7
-    # "Gram Positive Sepsis":"",
-    # "Fungal infection",
-    # "EDPD",
-    # "TAVR work up":"",
-    # "low hgb",
-    # "Generator change",
-    # "Pacemaker Problem",
-    # "Abd pain",
-    # "Enlarging hematoma to left chest wall",
-    # "Pump thrombosis",
-    # "Bacteremia/Mitral Vegetation": "I34.0",
-    # "Valvular heart failure": "I38",
 
-    # extra:
-    # "EXTRACTION, ELECTRODE LEAD, CARDIAC, USING LASER; \Reimplant of CRT-D with new RV and LV leads"
-    # "liver biopsy VAD patient", "CABG", "VAD patient for generator change Monday", "REMOVAL, ELECTRODE LEAD, ICD [1072379]", "VAD work- up", "Heart tx", "NSTEMI/CABG", "REMOVAL, ELECTRODE LEAD, ICD [1072379]", "NSTEMI/wtg CABG"
+def clean_or_proc_orders(df):
+    # drop all columns except STUDY_ID, ENCOUNTER_NUM, and OR_PROC_ID
+    df.drop(['PROC_DISPLAY_NAME', 'ANESTHESIA_START_TOD', 'ANESTHESIA_START_HRS_FROM_ADMIT', 'PROCEDURE_START_TOD', 'PROCEDURE_START_HRS_FROM_ADMIT',
+            'PROCEDURE_COMP_TOD', 'PROCEDURE_COMP_HRS_FROM_ADMIT', 'ANESTHESIA_STOP_TOD', 'ANESTHESIA_STOP_HRS_FROM_ADMIT'], axis=1, inplace=True)
+
+
+def clean_orders_activiy(df):
+    # drop PROC_NAME, ORDER_PROC_ID
+    df.drop(['PROC_NAME', 'ORDER_PROC_ID'], axis=1, inplace=True)
+
+
+def clean_orders_nutrition(df):
+    # drop PROC_NAME, ORDER_PROC_ID
+    df.drop(['PROC_NAME', 'ORDER_PROC_ID'], axis=1, inplace=True)
+
+    # find/remove rows with empty DISCON_HRS
+    empty_discon = df[(df['ORDER_DISCON_TOD'].notnull()) == False].index
+
+    df.drop(empty_discon, axis=0, inplace=True)
+
 
 def clean_labs(df):
-    # The ratio of missing data is really small
-    # drop all the None value
-    return pd.DataFrame.dropna(df)
+    pd.DataFrame.dropna(df)
+    # drop COMPONENT_NAME, EXTERNAL_NAME, REFERENCE_UNIT
+    df.drop(['COMPONENT_NAME', 'EXTERNAL_NAME',
+            'REFERENCE_UNIT'], axis=1, inplace=True)
+
+    # find/remove rows with empty ORD_VALUE
+    empty_ord = df[(df['ORD_VALUE'].notnull()) == False].index
+
+    df.drop(empty_ord, axis=0, inplace=True)
 
 
 def clean_med_admin(df):
+    '''
+    H02AB02
+    DEXAMETHASONE IN NACL 0.9% 50 ML 6000637
 
-    # drop ATC codes
-    df.drop('MEDICATION_ATC', axis=1, inplace=True)
+    H02AB09
+    HYDROCORTISONE SODIUM SUCCINATE IN NACL 0.9% 100 ML (100 MG VIAL)(FLOOR) 6003152
+    HYDROCORTISONE SODIUM SUCCINATE IN NACL 0.9% 50 ML (100 MG VIAL)(FLOOR) 6003151
+    HYDROCORTISONE SODIUM SUCCINATE IN NACL 0.9% 50 ML BAG 6001118
+
+    A10AB05
+    INSULIN ASPART (NOVORAPID FLEXTOUCH) 100 UNIT/ML INJECTION PEN 4002908
+
+    A10AB04
+    INSULIN LISPRO (HUMALOG KWIKPEN) 100 UNIT/ML INJECTION PEN 4002884
+    INSULIN LISPRO (HUMALOG) IN NACL 0.9% 50 ML BAG (FLOOR) 6001625
+
+    A10AB01
+    ZZZ_INSULIN REGULAR (HUMULIN R) 1 UNIT/ML IN NACL 0.9% 100 ML (RN) 6004606
+    INSULIN REGULAR (HUMULIN R) 1 UNIT/ML (100 UNIT) IN NACL 0.9% 100 ML INFUSION BAG 6002910
+
+    H02AB04
+    ZZZMETHYLPREDNISOLONE SODIUM SUCCINATE IN D5W 50 ML BAG (125 MG/2 ML VIAL)(RN) 6003166
+    '''
+
+    # ATC code fixes
+    dexamethasone = df[df['MEDICATION_ID'] == 6000637].index
+    for i in range(len(dexamethasone)):
+        df.loc[dexamethasone[i], "MEDICATION_ATC"] = 'H02AB02'
+
+    hydrocortisone = df[df['MEDICATION_ID'] == 6003152].index
+    for i in range(len(hydrocortisone)):
+        df.loc[hydrocortisone[i], "MEDICATION_ATC"] = 'H02AB09'
+    hydrocortisone = df[df['MEDICATION_ID'] == 6003151].index
+    for i in range(len(hydrocortisone)):
+        df.loc[hydrocortisone[i], "MEDICATION_ATC"] = 'H02AB09'
+    hydrocortisone = df[df['MEDICATION_ID'] == 6001118].index
+    for i in range(len(hydrocortisone)):
+        df.loc[hydrocortisone[i], "MEDICATION_ATC"] = 'H02AB09'
+
+    insulin_aspart = df[df['MEDICATION_ID'] == 4002908].index
+    for i in range(len(insulin_aspart)):
+        df.loc[insulin_aspart[i], "MEDICATION_ATC"] = 'A10AB05'
+
+    insulin_lispro = df[df['MEDICATION_ID'] == 4002884].index
+    for i in range(len(insulin_lispro)):
+        df.loc[insulin_lispro[i], "MEDICATION_ATC"] = 'A10AB04'
+    insulin_lispro = df[df['MEDICATION_ID'] == 6001625].index
+    for i in range(len(insulin_lispro)):
+        df.loc[insulin_lispro[i], "MEDICATION_ATC"] = 'A10AB04'
+
+    insulin_regular = df[df['MEDICATION_ID'] == 6004606].index
+    for i in range(len(insulin_regular)):
+        df.loc[insulin_regular[i], "MEDICATION_ATC"] = 'A10AB01'
+    insulin_regular = df[df['MEDICATION_ID'] == 6002910].index
+    for i in range(len(insulin_regular)):
+        df.loc[insulin_regular[i], "MEDICATION_ATC"] = 'A10AB01'
+
+    methylprednisolone = df[df['MEDICATION_ID'] == 6003166].index
+    for i in range(len(methylprednisolone)):
+        df.loc[methylprednisolone[i], "MEDICATION_ATC"] = 'H02AB04'
+
+    # infusion unit fixes
+    inf_unit_fixes = df[df['INFUSION_RATE_UNIT'] == 'mL/hr'].index
+    for i in range(len(inf_unit_fixes)):
+        # fix infusion rate == 0
+        if df.loc[inf_unit_fixes[i], "INFUSION_RATE"] == 0:
+            df.loc[inf_unit_fixes[i], "SIG"] = 0
+            df.loc[inf_unit_fixes[i], "DOSE_UNIT"] = 'mL/hr'
+        # fix empty values in SIG
+        elif pd.isnull(df.loc[inf_unit_fixes[i], "SIG"]):
+            df.loc[inf_unit_fixes[i],
+                   "SIG"] = df.loc[inf_unit_fixes[i], "INFUSION_RATE"]
+            df.loc[inf_unit_fixes[i], "DOSE_UNIT"] = 'mL/hr'
+        # normalize units
+        elif df.loc[inf_unit_fixes[i], "DOSE_UNIT"] not in ['g', 'mL', 'mg']:
+            df.loc[inf_unit_fixes[i], "DOSE_UNIT"] = 'mL/hr'
 
     # missing routes for meds
     med_routes = {4000287: "oral", 124838: "subcutaneous", 2365: "intravenous", 4002245: "intravenous",
@@ -133,18 +249,30 @@ def clean_med_admin(df):
     # insulin_list = ["17405", "28534", "30080", "124838", "124845", "124847", "124854", "124857", "125482", "130342", "134056", "162674", "166114", "169138", "199429", "4002243",
     #                "4002245", "4002455", "4002541", "4002722", "4002723", "4002884", "4002908", "4002909", "6000598", "6001625", "6002910", "6004503", "6004606"]
 
-    # QUESTION: what to do when columns I-M are empty?
-
     # get rows where route is empty
     list = df[(df['ROUTE'].notnull()) == False].index
 
+    # fill in empty values
     for i in range(len(list)):
-        df.loc[list[i], "CURRENT_ICD10_LIST"] = med_routes[df.loc[list[i],
-                                                                  "MEDICATION_ID"]]    # fill in missing routes
+        df.loc[list[i], "ROUTE"] = med_routes[df.loc[list[i], "MEDICATION_ID"]]
+
+    # find/remove rows with empty values
+    empty_vals = df[(df['SIG'].notnull()) == False].index
+    df.drop(empty_vals, axis=0, inplace=True)
+
+    # drop columns
+    df.drop(['MEDICATION_ID', 'MEDICATION_NAME', 'INFUSION_RATE',
+            'INFUSION_RATE_UNIT', 'STRENGTH'], axis=1, inplace=True)
+
+
+def clean_pin(df):
+    # drop everything but STUDY_ID, DISP_DAYS_PRIOR, SUPP_DRUG_ATC_CODE
+    df.drop(['ENCOUNTER_NUM', 'DRUG_DIN', 'BRAND_NAME', 'DSPN_AMT_QTY', 'DSPN_AMT_UNT_MSR_CD',
+            'DSPN_DAY_SUPPLY_QTY', 'DSPN_DAY_SUPPLY_UNT_MSR_CD'], axis=1, inplace=True)
 
 
 def write_to_csv(df_file, name):
-    df_file.to_csv(name+".csv", index=None, header=True)
+    df_file.to_csv("data/"+name+".csv", index=None, header=True)
 
 
 def preprocess_data(df_file, name):
